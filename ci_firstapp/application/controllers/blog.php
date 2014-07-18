31989d4375278efdcfd3c62d53268d66d5d940b7
Son Huy TRAN <sonhuytran@gmail.com>
2014-07-18 16:08:21 +0200

    CI_FirstApp

---
+++ ci_firstapp/application/controllers/blog.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Huy
+ * Date: 18/07/14
+ * Time: 14:41
+ */
+
+if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Blog extends CI_Controller {
+
+    public function index() {
+        echo "Hello World!";
+    }
+} 
